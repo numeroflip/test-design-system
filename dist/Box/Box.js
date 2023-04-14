a944cec32df4824b9185b208dcc9c18f6e8ddf3f
Aron <aron.berenyi@tuta.io>
2023-04-14 12:44:07 +0200

    use umd

--- dist/Box/Box.js
+++ dist/Box/Box.js
@@ -1,2 +1,15 @@
-import { Box as MUIBox } from "@material-ui/core";
-export var Box = MUIBox;
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "@material-ui/core"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.Box = void 0;
+    var core_1 = require("@material-ui/core");
+    exports.Box = core_1.Box;
+});
